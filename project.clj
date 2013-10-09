4a1a2a11549ffdcc6ec5f924c2d2edeb547cab13
Nikita Beloglazov <nikelandjelo@gmail.com>
2013-10-10 01:08:10 +0300

    Initial version of the plugin.

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject lein-garden "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[watchtower "0.1.1"]
+                 [me.raynes/fs "1.4.4"]
+                 [garden "1.1.0"]
+                 [org.clojure/tools.reader "0.7.8"]]
+  :eval-in-leiningen true)
---
+++ src/leiningen/garden.clj
@@ -0,0 +1,124 @@
+(ns leiningen.garden
+  (:require [clojure.pprint :refer [pprint]]
+            [me.raynes.fs :as fs]
+            [watchtower.core :as wc]
+            [clojure.java.io :as io]
+            [clojure.tools.reader :as r]
+            [clojure.tools.reader.reader-types :as rt]
+            [garden.core :as gc]
+            garden.types
+            [clojure.string :as string]))
+
+(defn- get-dir [project type]
+  (fs/file (:root project) (get-in project [:garden type])))
+
+(def cssable?
+  (some-fn vector? #(instance? garden.types.CSSAtRule %)))
+
+(def date-format (java.text.SimpleDateFormat. "HH:mm:ss"))
+
+(defn time-now []
+  (.format date-format (java.util.Date.)))
+
+(defmacro with-temp-ns [& body]
+  (let [body (cons '(clojure.core/refer 'clojure.core)
+                     body)]
+   `(let [~'base-ns (ns-name *ns*)]
+      (try
+        (in-ns 'temp-ns#)
+        ~@(map (fn [exp] `(eval '~exp)) body)
+        (finally
+          (let [~'cur-ns (ns-name *ns*)]
+            (in-ns  ~'base-ns)
+            ; Just in case if user changed ns in his garden file
+            ; delete his custom ns
+            (when-not (= ~'cur-ns ~'base-ns)
+              (remove-ns ~'cur-ns))
+            (remove-ns 'temp-ns#)))))))
+
+(defn- cssable-from-reader [reader]
+  (loop [css []]
+    (if-let [exp (r/read reader false nil)]
+      (let [obj (eval exp)]
+        (if (cssable? obj)
+          (recur (conj css obj))
+          (recur css)))
+      css)))
+
+(defn- cssable-from-file [file]
+  (-> file
+      fs/file
+      io/input-stream
+      rt/input-stream-push-back-reader
+      rt/indexing-push-back-reader
+      cssable-from-reader))
+
+(defn- path-relative-to [file dir]
+  (let [dir-path (fs/absolute-path dir)
+        file-path (fs/absolute-path file)]
+    (assert (.startsWith file-path dir-path))
+    (subs file-path (inc (count dir-path)))))
+
+(defn- change-extension [file new-ext]
+  (let [path (fs/absolute-path file)]
+    (string/replace path #"\.\w+$" new-ext)))
+
+(defn- render-single-file [src-dir out-dir file]
+  (println (time-now) "compiling" (fs/base-name file))
+  (let [out-file (fs/file out-dir (path-relative-to file src-dir))
+        rules (cssable-from-file file)]
+    (fs/mkdirs (fs/parent out-file))
+    (gc/css {:output-to (change-extension out-file ".css")}
+            rules))
+  (println))
+
+(defn- render-files [src-dir out-dir files]
+  (doseq [file files]
+    (render-single-file src-dir out-dir file)))
+
+(defn- once
+  "Transform garden files to css once and then exit."
+  [src-dir out-dir]
+  (println "Once")
+  (render-files (fs/find-files* src-dir fs/file?)))
+
+(defn- auto
+  "Watch garden files and transform them to css after any changes."
+  [src-dir out-dir]
+  (println "Auto")
+  (wc/watcher [src-dir]
+    (wc/rate 1000)
+    (wc/on-change #(render-files src-dir out-dir %))
+    (wc/watch)))
+
+(defn- valid-project? [project]
+  (let [required [[[:garden]
+                   ":garden config not found.\ncheck project.clj"]
+                  [[:garden :source-path]
+                   ":source-path not found in :garden config.\ncheck project.clj"]
+                  [[:garden :output-path]
+                   ":output-path not found in :garden config.\ncheck project.clj"]]
+        errors (for [[keys mess] required
+                        :when (not (get-in project keys))]
+                 mess)]
+    (if (empty? errors)
+      true
+      (do (println (first errors)) false))))
+
+(defn garden
+  "Transform garden files to css."
+  {:subtasks [#'once #'auto]}
+  ([project] (garden project "once"))
+  ([project subtask]
+     (when (valid-project? project)
+      (let [src-dir (get-dir project :source-path)
+            out-dir (get-dir project :output-path)]
+       (println src-dir)
+       (println out-dir)
+       (case subtask
+        "once" (once src-dir out-dir)
+        "auto" (auto src-dir out-dir))))))
+
+;;(cssable-from-file "src/garden/main.clj")
+
+
